a7118772d1461745041e2de3eb347abfff5130da
Sahil Athia <sahilathia918@gmail.com>
2021-02-25 21:49:41 +0000

    add link to sign-up form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://api.memberstack.io/static/memberstack.js?custom";
+    script.setAttribute('data-memberstack-id', '437974800f22566d2a57ac00bdc867ef');
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="#/ms/signup/60381a660dd8ef0004dd0f58">Sign-Up</a>
     </div>
   );
 }
